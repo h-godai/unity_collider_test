5f9cca98f8b80cb99dbdaec5627e2f7741c1f3ec
KOHEI IKEDA <godai@techarts.co.jp>
2019-06-24 03:06:24 +0900

    first commit

---
+++ ColliderTeest_Unity2019.1.7/Assets/Scenes/ShotTheBall.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotTheBall : MonoBehaviour
+{
+    public Transform BallPrefab;
+    public int BallCount = 30;
+    public float LeftLine = -5.0f;
+    public float RightLine = 5.0f;
+    public float ShotZ = -10.0f;
+    public float ShotHeight = 1.0f;
+    public float ShotAngleStart = 0.0f;
+    public float ShotAngleEnd = 30.0f;
+    public float ShotVerocity = 40.0f;
+    private int frame = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Time.timeScale = 1.0f;
+        Time.fixedDeltaTime = 1.0f / 72.0f;        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 72;       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.frame < this.BallCount)
+        {
+            var progress = (float)this.frame / (float)this.BallCount;
+            var x = this.LeftLine + progress * (this.RightLine - this.LeftLine);
+            var vy = this.ShotAngleStart + progress * (this.ShotAngleEnd - this.ShotAngleStart);
+            var ball = Instantiate(this.BallPrefab);
+            ball.position = new Vector3(x, this.ShotHeight, this.ShotZ);
+            ball.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, vy, this.ShotVerocity), ForceMode.Impulse);
+        }
+        ++this.frame;
+    }
+}
---
+++ ColliderTeest_Unity2019.1.7/Assets/Scenes/ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public Transform BeforeBallPrefab;
+
+    private Transform beforeBall;
+    private Vector3 lastPosition = Vector3.zero;
+    private void Awake()
+    {
+        this.beforeBall = Instantiate(this.BeforeBallPrefab);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!this.GetComponent<Rigidbody>().isKinematic && this.transform.position.z < 0.0f)
+        {
+            this.lastPosition = this.transform.position;
+        }
+        this.beforeBall.position = this.lastPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        this.GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
