2394c0089a1766d2760104f086304db39f4de675
Kohei Ikeda <godai@techarts.co.jp>
2019-06-25 14:28:07 +0900

    原因究明。FixedUpdateの頻度によって結果が変わる。50だと安定する。

--- ColliderTeest_Unity2019.1.7/Assets/Scenes/ShotTheBall.cs
+++ ColliderTeest_Unity2019.1.7/Assets/Scenes/ShotTheBall.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class ShotTheBall : MonoBehaviour
 {
     public Transform BallPrefab;
-    public int BallCount = 30;
-    public float LeftLine = -5.0f;
-    public float RightLine = 5.0f;
-    public float ShotZ = -10.0f;
-    public float ShotHeight = 1.0f;
-    public float ShotAngleStart = 0.0f;
-    public float ShotAngleEnd = 30.0f;
-    public float ShotVerocity = 40.0f;
+    public int BallCount = 150;
+    public float LeftLine = -10.0f;
+    public float RightLine = 10.0f;
+    public float ShotZ = -5.0f;
+    public float ShotHeight = 0.5f;
+    public float ShotAngleStart = 0.39f;
+    public float ShotAngleEnd = 0.43f;
+    public float ShotVerocity = 3.0f;
+    public int FrameRate = 72;
+    public int FixedUpdateRate = 72;
+    public float TimeScale = 1.0f;
+
     private int frame = 0;
     
     // Start is called before the first frame update
     void Start()
     {   
-        Time.timeScale = 1.0f;
-        Time.fixedDeltaTime = 1.0f / 72.0f;        
+        Time.timeScale = this.TimeScale;;
+        Time.fixedDeltaTime = 1.0f / (float)this.FixedUpdateRate;        
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 72;       
+        Application.targetFrameRate = this.FrameRate;       
     }
 
     // Update is called once per frame
--- ColliderTeest_Unity2019.1.7/Assets/Scenes/ball.cs
+++ ColliderTeest_Unity2019.1.7/Assets/Scenes/ball.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class ball : MonoBehaviour
 {
     public Transform BeforeBallPrefab;
+    public Transform CollisionBallPrefab;
 
     private Transform beforeBall;
     private Vector3 lastPosition = Vector3.zero;
+    private Rigidbody Rigidbody;
+    private bool hasCollision = false;
     private void Awake()
     {
         this.beforeBall = Instantiate(this.BeforeBallPrefab);
+        this.Rigidbody = this.GetComponent<Rigidbody>();
     }
 
     // Start is called before the first frame update
@@ -22,15 +26,26 @@ public class ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!this.GetComponent<Rigidbody>().isKinematic && this.transform.position.z < 0.0f)
+        if (!this.hasCollision)
         {
             this.lastPosition = this.transform.position;
         }
         this.beforeBall.position = this.lastPosition;
     }
 
+
     private void OnCollisionEnter(Collision collision)
     {
-        this.GetComponent<Rigidbody>().isKinematic = true;
+        var obj = Instantiate(this.CollisionBallPrefab);
+        obj.transform.position = this.transform.position;
+        this.hasCollision = true;
+        if (this.transform.position.z > 0.0f && this.transform.position.y > 0.5f)
+        {
+            foreach (var c in collision.contacts)
+            {
+                Debug.DrawRay(c.point, c.normal, Color.red);
+                Debug.Log($"OnCollisionEnter: {c.point} {c.normal} {c.separation}");
+            }
+        }
     }
 }
